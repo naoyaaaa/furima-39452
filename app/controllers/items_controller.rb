f48e5d54c5b20ecee50bc68400b3b4ba852cf6c2
naoyaaaa <23nakajima.naoya@gmail.com>
2023-06-19 18:03:58 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,10 @@ class ItemsController < ApplicationController
   def index
     # @items = Item.all
   end
+
+  zprivate
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
