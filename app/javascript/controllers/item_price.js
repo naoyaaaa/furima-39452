b9f8ac149504fe265be3585c7ca7be88ba25b743
naoyaaaa <23nakajima.naoya@gmail.com>
2023-06-21 12:51:59 +0900

    追加修正

--- app/javascript/controllers/item_price.js
+++ app/javascript/controllers/item_price.js
@@ -2,7 +2,6 @@ const priceInput = document.getElementById("item-price");
 const addTaxDom = document.getElementById("add-tax-price");
 const profitDom = document.getElementById("profit");
 
-// 初期表示時に手数料と利益を計算して表示
 calculatePrice();
 
 priceInput.addEventListener("input", () => {
@@ -12,20 +11,19 @@ priceInput.addEventListener("input", () => {
 function calculatePrice() {
   const inputValue = priceInput.value;
 
-  // 価格の入力値が数値でない場合や範囲外の場合は手数料と利益を空欄にリセット
   if (!inputValue || isNaN(inputValue) || !isValidPrice(inputValue)) {
     addTaxDom.textContent = "";
     profitDom.textContent = "";
     return;
   }
 
-  const price = parseInt(inputValue, 10); // 入力値を整数に変換
+  const price = parseInt(inputValue, 10); 
 
-  // 手数料を計算して表示
-  const tax = Math.floor(price * 0.1); // 10% の手数料（小数点以下切り捨て）
+
+  const tax = Math.floor(price * 0.1); 
   addTaxDom.textContent = tax;
 
-  // 販売利益を計算して表示
+
   const profit = price - tax;
   profitDom.textContent = profit;
 }
@@ -37,7 +35,7 @@ function isValidPrice(value) {
   return value >= minPrice && value <= maxPrice;
 }
 
-// ページがリロードされたときに価格を復元する
+
 window.addEventListener("DOMContentLoaded", () => {
   calculatePrice();
 });
