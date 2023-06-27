ded016997c9a913b79f2d8545001aa28935766f7
naoyaaaa <23nakajima.naoya@gmail.com>
2023-06-27 17:51:34 +0900

    再修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one :purchase_log
+  #has_one :purchase_log
   has_one_attached :image
 
   validates :item_name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users 
   root to:"items#index"
-  resources :users
+  #resources :users
   resources :items
-  
+
 end
