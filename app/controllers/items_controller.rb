230579b2a81b14f712cf5bd24247e738f52e8bcc
naoyaaaa <23nakajima.naoya@gmail.com>
2023-06-30 15:49:54 +0900

    出品者以外のユーザーを弾く

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,12 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
+
+    if item.user != current_user
+      redirect_to root_path
+      return
+    end
+
     item.destroy
     redirect_to root_path
   end
